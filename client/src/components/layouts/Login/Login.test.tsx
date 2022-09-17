5040d061b37731ebd3799181916648b83f22b519
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-17 17:19:33 +0530

    [tests]: Implemented test cases for  messages to be displayed on form validation

--- client/src/components/layouts/Login/Login.test.tsx
+++ client/src/components/layouts/Login/Login.test.tsx
@@ -2,7 +2,7 @@
 import { MemoryRouter } from "react-router-dom";
 
 // React Testing utils
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 // Subject Component
 import Login from "./index";
@@ -34,5 +34,64 @@ describe('Login Page', (): void => {
         expect(rememberChkBox).toBeInTheDocument();
         expect(submitBtn).toBeInTheDocument();
 
-     });
+    });
+
+    // Test case - 2
+    test('Error is displayed for required fields', async (): Promise<void> => {
+        // Arrange
+        // render the component in jest environment
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+
+        // Query elements
+        const submitBtn = screen.getByTestId('submit-btn');
+        
+        // submit the form
+        fireEvent.click(submitBtn);
+        
+        // error is displayed for skipping required fields
+        await waitFor((): void => {
+            const emailErr = screen.getByTestId('email-error');
+            expect(emailErr.innerHTML.toLocaleLowerCase()).toBe('email is required!');
+        });
+
+        await waitFor((): void => {
+            const passwordErr = screen.getByTestId('password-error');
+            expect(passwordErr.innerHTML.toLocaleLowerCase()).toBe('password is required!');
+        });
+    });
+
+    // Test Case 3
+    test('Validation messages are displayed on invalid input', async (): Promise<void> => {
+        // Arrange
+        // render the component in jest environment
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+
+        const invalidEmail = "a@b";
+        const invalidEmailErrMsg = "Invalid Email Format";
+
+        // Query elements
+        const emailEl = screen.getByTestId('email') as HTMLInputElement;
+        const submitBtn = screen.getByTestId('submit-btn');
+
+        // Fire events
+        fireEvent.change(emailEl, { target: { value: invalidEmail } });
+        fireEvent.click(submitBtn);
+
+        // Assertions
+        await waitFor((): void => expect(emailEl.value).toBe(invalidEmail));
+        // check for error message
+        await waitFor((): void => {
+            const emailErrorEl = screen.getByTestId('email-error');
+            expect(emailErrorEl.innerHTML).toBe(invalidEmailErrMsg);
+        });
+        
+    });
 });
