9ac6a056f93ce8eb6b824a10a6779d5830204ee5
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-17 22:50:26 +0530

    [feat]: Implemented MSW and handlers to test API calls

--- client/src/mocks/setup.ts
+++ client/src/mocks/setup.ts
@@ -1,4 +1,9 @@
-import { setupWorker } from "msw";
+// import { setupWorker } from "msw";
+import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 
-export const worker = setupWorker(...handlers);
+// worker to mock APIs in browser
+// export const worker = setupWorker(...handlers);
+
+// mock server to intercept API calls in test env
+export const server = setupServer(...handlers);
